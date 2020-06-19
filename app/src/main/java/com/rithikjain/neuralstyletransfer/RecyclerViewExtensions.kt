20eb59a67e2259bad1b1bdb667a348b316d269f9
Rithik Jain <rithik.jain3006@gmail.com>
2020-06-19 17:32:16 +0530

    Removed onLong Click listener from recycler view

--- app/src/main/java/com/rithikjain/neuralstyletransfer/RecyclerViewExtensions.kt
+++ app/src/main/java/com/rithikjain/neuralstyletransfer/RecyclerViewExtensions.kt
@@ -21,19 +21,3 @@ fun RecyclerView.addOnItemClickListener(onClickListener: OnItemClickListener) {
         }
     })
 }
-
-fun RecyclerView.addOnItemLongClickListener(onClickListener: OnItemClickListener) {
-    this.addOnChildAttachStateChangeListener(object: RecyclerView.OnChildAttachStateChangeListener {
-        override fun onChildViewDetachedFromWindow(view: View) {
-            view.setOnClickListener(null)
-        }
-
-        override fun onChildViewAttachedToWindow(view: View) {
-            view.setOnLongClickListener{
-                val holder = getChildViewHolder(view)
-                onClickListener.onItemClicked(holder.adapterPosition, view)
-                true
-            }
-        }
-    })
-}
