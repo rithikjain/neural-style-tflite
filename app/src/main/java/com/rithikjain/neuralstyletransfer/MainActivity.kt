5c382fc2f62da112eb786e1f0a0eff3530309e82
Rithik Jain <rithik.jain3006@gmail.com>
2020-06-19 21:29:33 +0530

    Figured out download mostly

--- app/src/main/java/com/rithikjain/neuralstyletransfer/MainActivity.kt
+++ app/src/main/java/com/rithikjain/neuralstyletransfer/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.rithikjain.neuralstyletransfer
 
 import android.app.Activity
+import android.content.Context
 import android.content.Intent
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
+import android.graphics.drawable.BitmapDrawable
 import android.os.Bundle
+import android.os.Environment
 import android.util.Log
 import android.view.View
 import android.widget.Toast
@@ -17,6 +20,9 @@ import com.theartofdev.edmodo.cropper.CropImage
 import com.theartofdev.edmodo.cropper.CropImageView
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.coroutines.*
+import java.io.File
+import java.io.FileOutputStream
+import java.util.*
 import kotlin.coroutines.CoroutineContext
 
 class MainActivity : AppCompatActivity(), CoroutineScope {
@@ -55,12 +61,28 @@ class MainActivity : AppCompatActivity(), CoroutineScope {
 
         styleTransferModel = StyleTransferModel(this)
 
-        runWithPermissions(android.Manifest.permission.READ_EXTERNAL_STORAGE) {
+        runWithPermissions(
+            android.Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            android.Manifest.permission.READ_EXTERNAL_STORAGE
+        ) {
             selectImageButton.setOnClickListener {
                 CropImage.activity()
                     .setGuidelines(CropImageView.Guidelines.ON)
                     .setAspectRatio(1, 1)
                     .start(this)
+            }
+        }
+
+        saveImageButton.setOnClickListener {
+            if (!isModelRunning) {
+                val imgDrawable = outputImage.drawable
+                if (imgDrawable != null) {
+                    val imgTemp = outputImage.drawable as BitmapDrawable
+                    val imgBitmap = imgTemp.bitmap
+                    saveImage(imgBitmap, this)
+                } else {
+                    Toast.makeText(this, "Output image is empty", Toast.LENGTH_SHORT).show()
+                }
             }
         }
 
@@ -121,6 +143,26 @@ class MainActivity : AppCompatActivity(), CoroutineScope {
             } else if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE) {
                 Log.d("esh", "Error ${result.error}")
             }
+        }
+    }
+
+    private fun saveImage(imgBitmap: Bitmap, context: Context) {
+        try {
+            val pathStr =
+                Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).absolutePath
+            val path = File(pathStr)
+            if (!path.isDirectory || !path.exists()) {
+                path.mkdirs()
+            }
+            val outFile = File(path, "${UUID.randomUUID()}.png")
+            val stream = FileOutputStream(outFile)
+            imgBitmap.compress(Bitmap.CompressFormat.PNG, 100, stream)
+            stream.flush()
+            stream.close()
+            Toast.makeText(context, "Image saved", Toast.LENGTH_SHORT).show()
+        } catch (e: Exception) {
+            Log.d("esh", "File error $e")
+            Toast.makeText(context, "Could not be saved", Toast.LENGTH_SHORT).show()
         }
     }
 
